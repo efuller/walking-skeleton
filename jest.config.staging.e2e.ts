59a9470f7b2900db29ce73812f72242ccbbbc84f
Eric Fuller <info@glassmoonmedia.com>
2023-12-16 08:34:45 -0500

    Fix staging config

--- jest.config.staging.e2e.ts
+++ jest.config.staging.e2e.ts
@@ -2,5 +2,5 @@ import { Config } from 'jest';
 
 export default async (): Promise<Config> => ({
   verbose: true,
-  projects: ['./packages/api/jest.config.e2e.ts']
+  projects: ['./packages/api/jest.config.staging.e2e.ts']
 })
