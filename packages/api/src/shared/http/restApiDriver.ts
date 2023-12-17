59719f9c94f99d0777cbb4cf23711892ba6ef165
Eric Fuller <info@glassmoonmedia.com>
2023-12-17 11:38:36 -0500

    Remove hardcoded URL

--- packages/api/src/shared/http/restApiDriver.ts
+++ packages/api/src/shared/http/restApiDriver.ts
@@ -5,12 +5,12 @@ export class RestApiDriver {
   constructor(private http: Server) {}
 
   get(path: string) {
-    const response =  request('https://walking-skeleton-api.onrender.com').get(path);
+    const response =  request(this.http).get(path);
     return response;
   }
 
   post(path: string, data: Record<string, any>) {
-    const response =  request('https://walking-skeleton-api.onrender.com').post(path).set('Accept', 'application/json').send(data);
+    const response =  request(this.http).post(path).set('Accept', 'application/json').send(data);
     return response;
   }
 }
