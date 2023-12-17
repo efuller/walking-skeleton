f7447aedf60836f508d9423050790671978ad1a3
Eric Fuller <info@glassmoonmedia.com>
2023-12-17 13:28:16 -0500

    Combine jest config files for staging

--- jest.config.staging.ts
+++ jest.config.staging.ts
@@ -2,5 +2,5 @@ import { Config } from 'jest';
 
 export default async (): Promise<Config> => ({
   verbose: true,
-  projects: ['./packages/api/jest.config.staging.infra.ts']
+  projects: ['./packages/api/jest.config.staging.ts']
 })
