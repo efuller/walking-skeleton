13473d9344aeaa3c198ad1ad6d8c89ca943cdb69
Eric Fuller <info@glassmoonmedia.com>
2023-12-19 19:07:08 -0500

    Use NODE_ENV to set env file and docker-compose file

--- packages/api/tests/setupLocalTestingEnv.ts
+++ packages/api/tests/setupLocalTestingEnv.ts
@@ -6,8 +6,9 @@ import { setupEnvVars } from '@efuller/shared/tests/utils/setupEnvVars';
 export default async () => {
   const environment = process.env.NODE_ENV || 'development';
   console.log('[setupInfraTestingEnv] environment: ', environment);
+
   const rootCwd = path.join(__dirname, './api');
-  await startDockerDB('docker-compose.test.yml', path.join(rootCwd, '../../'));
-  setupEnvVars('./packages/api/.env.test');
-  await generatePrismaClient('.env.test', rootCwd);
+  await startDockerDB(`docker-compose.${environment}.yml`, path.join(rootCwd, '../../'));
+  setupEnvVars(`./packages/api/.env.${environment}`);
+  await generatePrismaClient(`.env.${environment}`, rootCwd);
 };
