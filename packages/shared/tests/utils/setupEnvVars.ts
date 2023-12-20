f89986f0cf832c48d1834b90bfe407f81be61ae0
Eric Fuller <info@glassmoonmedia.com>
2023-12-19 19:26:06 -0500

    More debugging

--- packages/shared/tests/utils/setupEnvVars.ts
+++ packages/shared/tests/utils/setupEnvVars.ts
@@ -6,9 +6,9 @@ export const setupEnvVars = (envFile: string) => {
   console.log('Loading env vars from ', envFile);
   console.log('-----------------------------------');
 
-  console.log('----Resolved path----', path.resolve(envFile));
+  console.log('----Resolved path----', path.join(envFile, '../'));
 
-  dotenv.config({ path: path.resolve(envFile) });
+  dotenv.config({ path: path.join(envFile, '../') });
 
   console.log('-----------------------------------');
   console.log(`The connection URL for the DB is: ${process.env.DATABASE_URL}`);
