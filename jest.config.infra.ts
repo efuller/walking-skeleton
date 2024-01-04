e5f14c1f5c1cba5b5cffb28e0f4287498e7d5110
Eric Fuller <info@glassmoonmedia.com>
2024-01-04 18:20:42 -0500

    Rename integration back to infra

--- jest.config.infra.ts
+++ jest.config.infra.ts
@@ -2,5 +2,5 @@ import { Config } from 'jest';
 
 export default async (): Promise<Config> => ({
   verbose: true,
-  projects: ['./packages/api/jest.config.integration.ts']
+  projects: ['./packages/api/jest.config.infra.ts']
 })
--- packages/api/jest.config.infra.ts
+++ packages/api/jest.config.infra.ts
@@ -2,7 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 export default async (): Promise<JestConfigWithTsJest> => ({
   displayName: 'Backend (infra)',
-  testMatch: ['**/@(src|tests)/**/*.@(itest|ispec).*'],
+  testMatch: ['**/@(src|tests)/**/*.@(infra).*'],
   transform: {
     '^.+\\.(t|j)sx?$': ['ts-jest', {}],
   },
