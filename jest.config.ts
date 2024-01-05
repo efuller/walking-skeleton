29048720307901408bbf3ba77ea664f7cd273631
Eric Fuller <info@glassmoonmedia.com>
2024-01-04 19:16:19 -0500

    Consolidate staging e2e and infra tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
+import path from 'path';
 import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
 
 export default async (): Promise<Config> => ({
   verbose: true,
@@ -31,6 +35,30 @@ export default async (): Promise<Config> => ({
       },
       rootDir: '<rootDir>/packages/api',
       globalSetup: './tests/setupLocalTestingEnv.ts',
+    },
+    {
+      displayName: 'api-staging-e2e',
+      testMatch: ['**/@(src|tests)/**/*.@(e2e).*'],
+      transform: {
+        '^.+\\.(t|j)sx?$': ['ts-jest', {}],
+      },
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: path.resolve(__dirname, '../../'),
+      }),
+      rootDir: '<rootDir>/packages/api',
+      globalSetup: './tests/setupStagingTestingEnv.ts',
+    },
+    {
+      displayName: 'api-staging-infra',
+      testMatch: ['**/@(src|tests)/**/*.@(infra).*'],
+      transform: {
+        '^.+\\.(t|j)sx?$': ['ts-jest', {}],
+      },
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: path.resolve(__dirname, '../../'),
+      }),
+      rootDir: '<rootDir>/packages/api',
+      globalSetup: './tests/setupStagingTestingEnv.ts',
     }
   ]
 })
