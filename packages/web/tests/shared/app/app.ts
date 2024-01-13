a138894b6b0cc9bad322fc2759e54cc5e1d19ce7
Eric Fuller <info@glassmoonmedia.com>
2024-01-13 10:53:44 -0500

    Refactor web app class

--- packages/web/tests/shared/app/app.ts
+++ packages/web/tests/shared/app/app.ts
@@ -7,20 +7,27 @@ type Pages = {
 
 export class WebApp {
   pageDriver: PuppeteerPageDriver;
-  readonly baseUrl: string;
-  pages: Pages;
+  baseUrl: string | null = null;
+  pages: Pages | undefined;
 
-  public constructor(driver: PuppeteerPageDriver) {
+  private constructor(driver: PuppeteerPageDriver) {
     this.pageDriver = driver;
-    this.baseUrl = 'http://localhost:5173/';
-    this.pageDriver.page.setDefaultTimeout(10000);
-    this.pages = this.generatePages();
   }
 
-  private generatePages(): Pages {
-    return {
-      homePage: new HomePage(this.pageDriver, this.baseUrl),
-    };
+  static async create(driver: PuppeteerPageDriver) {
+    const app = new WebApp(driver);
+    app.baseUrl = 'http://localhost:5173/';
+    app.pageDriver.page.setDefaultTimeout(10000);
+    await app.generatePages();
+    return app;
+  }
+
+  async generatePages() {
+    if (!this.baseUrl) {
+      throw new Error('Base url has not been set');
+    }
+    const homePage = await HomePage.create(this.pageDriver, this.baseUrl);
+    this.pages = { homePage };
   }
 
   async close() {
@@ -28,14 +35,20 @@ export class WebApp {
   }
 
   async pause() {
-    await new Promise(r => setTimeout(r, 3000))
+    await new Promise((r) => setTimeout(r, 3000));
   }
 
   async navigateToHomepage() {
+    if (!this.pages) {
+      throw new Error('Pages have not been generated');
+    }
     await this.pages.homePage.navigate();
   }
 
   getPageObject<T extends keyof Pages>(pageName: T): Pages[T] {
+    if (!this.pages) {
+      throw new Error('Pages have not been generated');
+    }
     return this.pages[pageName];
   }
 }
