fb1c718998e63488209bcb130db4253e7b50407c
Eric Fuller <info@glassmoonmedia.com>
2024-01-18 19:34:49 -0500

    Create generic ApiResponse to get linter passing

--- packages/api/src/modules/journals/journal.service.ts
+++ packages/api/src/modules/journals/journal.service.ts
@@ -1,10 +1,15 @@
 import { Database } from '@efuller/api/src/shared/persistence/database/database';
 import { ApiResponse } from '@efuller/shared/src/api';
 
+export interface Journal {
+  title: string;
+  content: string;
+}
+
 export class JournalService {
   constructor(private readonly db: Database) {}
 
-  async createJournal(title: string, content: string): Promise<ApiResponse> {
+  async createJournal(title: string, content: string): Promise<ApiResponse<Journal>> {
     const dbClient = this.db.getClient();
 
     const result = await dbClient.journal.create({
@@ -16,7 +21,7 @@ export class JournalService {
 
     return {
       success: true,
-      data: { title: result.title, content: result.content },
+      data: { title: result.title, content: result.content ?? '' },
     }
   }
 }
