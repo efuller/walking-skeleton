5ac1735864328c407e78e282610fc7fe0b038b15
Eric Fuller <info@glassmoonmedia.com>
2024-06-26 20:26:27 -0400

    Initial install, setup, and introspect of DB for drizzle

---
+++ packages/api/src/shared/persistence/drizzle/schema.ts
@@ -0,0 +1,17 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core"
+import { sql } from "drizzle-orm"
+
+
+
+export const Journal = pgTable("Journal", {
+	id: text("id").primaryKey().notNull(),
+	title: text("title").notNull(),
+	content: text("content"),
+	createdAt: timestamp("createdAt", { precision: 3, mode: 'string' }).defaultNow().notNull(),
+	updatedAt: timestamp("updatedAt", { precision: 3, mode: 'string' }).notNull(),
+});
+
+export const DBHealth = pgTable("DBHealth", {
+	id: text("id").primaryKey().notNull(),
+	name: text("name"),
+});
