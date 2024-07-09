97bc72531fa1cd127b772fe9722a9f02b7bbfdb0
Eric Fuller <info@glassmoonmedia.com>
2024-07-08 21:01:34 -0400

    Remove comment

--- packages/api/setupDevEnv.ts
+++ packages/api/setupDevEnv.ts
@@ -3,7 +3,6 @@ import { startDockerDB } from '@efuller/shared/tests/utils/startDockerDB';
 import { setupEnvVars } from '@efuller/shared/tests/utils/setupEnvVars';
 import { generateDrizzleKit } from '@efuller/shared/tests/utils/generateDrizzleKit';
 
-// Let's move and organize things into shared. No need to have that top level test folder.
 export default async function setupDevEnv() {
   const rootCwd = path.join(__dirname);
   await startDockerDB('docker-compose.develop.yml', rootCwd);
