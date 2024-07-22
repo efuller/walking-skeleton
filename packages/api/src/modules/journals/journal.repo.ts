e18f1369329f84f1f3e1bad9241186c8b8427939
Eric Fuller <info@glassmoonmedia.com>
2024-07-22 19:58:59 -0400

    Update journal contract test

--- packages/api/src/modules/journals/journal.repo.ts
+++ packages/api/src/modules/journals/journal.repo.ts
@@ -4,4 +4,5 @@ import { CreateJournalDto, JournalDto } from '@efuller/api/src/shared/persistenc
 export interface JournalRepo {
   createJournal(user: CreateJournalDto): Promise<ApiResponse<JournalDto | null>>;
   getJournals(): Promise<ApiResponse<JournalDto[]>>;
+  getJournalById(id: number): Promise<ApiResponse<JournalDto | null>>;
 }
