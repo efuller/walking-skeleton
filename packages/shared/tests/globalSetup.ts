c5fda4136e6aedbda0314f7b96ca4403587bcc75
Eric Fuller <info@glassmoonmedia.com>
2024-07-25 19:35:41 -0400

    Remove ref to .development env

--- packages/shared/tests/globalSetup.ts
+++ packages/shared/tests/globalSetup.ts
@@ -2,31 +2,29 @@ import isCI from 'is-ci';
 import path from 'path';
 import execSh from 'exec-sh';
 import { generateDrizzleKit } from './utils/generateDrizzleKit';
-import { isPortAvailable } from './utils/detectPort';
+// import { isPortAvailable } from './utils/detectPort';
 import { setupEnvVars } from './utils/setupEnvVars';
 
 
 export default async () => {
   console.time('globalSetup');
-  if (isCI) {
-    setupEnvVars('./packages/api/.env.development');
-  } else {
+  if (!isCI) {
     setupEnvVars('./packages/shared/tests/.env.test');
   }
 
-  const port = Number(process.env.POSTGRES_PORT);
+  // const port = Number(process.env.POSTGRES_PORT);
 
-  const isDBPortAvailable = await isPortAvailable(port);
+  // const isDBPortAvailable = await isPortAvailable(port);
 
-  if (isDBPortAvailable) {
-    const out = await execSh.promise(
-      `supabase start -x realtime,storage-api,imgproxy,inbucket,postgrest,edge-runtime,logflare,vector,supavisor`,
-      {
-        cwd: path.join(__dirname, 'supabase'),
-      },
-    );
-    console.log(out.stdout, out.stderr);
-  }
+  // if (isDBPortAvailable) {
+  const out = await execSh.promise(
+    `supabase start -x realtime,storage-api,imgproxy,inbucket,postgrest,edge-runtime,logflare,vector,supavisor`,
+    {
+      cwd: path.join(__dirname, 'supabase'),
+    },
+  );
+  console.log(out.stdout, out.stderr);
+  // }
 
   await generateDrizzleKit(path.join(__dirname, '../../', 'api'));
 
