b8f5d08135201289019c7e272003dee8fb3ad33b
Eric Fuller <info@glassmoonmedia.com>
2024-07-25 20:49:39 -0400

    Remove port check

--- packages/shared/tests/globalSetup.ts
+++ packages/shared/tests/globalSetup.ts
@@ -2,7 +2,6 @@ import isCI from 'is-ci';
 import path from 'path';
 import execSh from 'exec-sh';
 import { generateDrizzleKit } from './utils/generateDrizzleKit';
-// import { isPortAvailable } from './utils/detectPort';
 import { setupEnvVars } from './utils/setupEnvVars';
 
 
@@ -12,11 +11,6 @@ export default async () => {
     setupEnvVars('./packages/shared/tests/.env.test');
   }
 
-  // const port = Number(process.env.POSTGRES_PORT);
-
-  // const isDBPortAvailable = await isPortAvailable(port);
-
-  // if (isDBPortAvailable) {
   const out = await execSh.promise(
     `supabase start -x realtime,storage-api,imgproxy,inbucket,postgrest,edge-runtime,logflare,vector,supavisor`,
     {
@@ -24,7 +18,6 @@ export default async () => {
     },
   );
   console.log(out.stdout, out.stderr);
-  // }
 
   await generateDrizzleKit(path.join(__dirname, '../../', 'api'));
 
