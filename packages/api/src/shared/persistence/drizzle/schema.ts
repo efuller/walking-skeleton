56c2c518fa2320e0c5aa286e6a19dfa3a8bfa42d
Eric Fuller <info@glassmoonmedia.com>
2024-07-29 19:44:48 -0400

    Add in a members table

--- packages/api/src/shared/persistence/drizzle/schema.ts
+++ packages/api/src/shared/persistence/drizzle/schema.ts
@@ -8,6 +8,19 @@ export const journal = pgTable("journal", {
 	updatedAt: timestamp("updated_at", { precision: 3, mode: 'string' }).defaultNow().notNull(),
 });
 
+export const members = pgTable("members", {
+	id: serial("id").primaryKey(),
+	firstName: varchar("first_name", { length: 50 }),
+	lastName: varchar("last_name", { length: 50 }),
+	email: varchar("email", { length: 255 }).notNull(),
+	password: text("password").notNull(),
+	createdAt: timestamp("created_at", { precision: 3, mode: 'string' }).defaultNow().notNull(),
+	updatedAt: timestamp("updated_at", { precision: 3, mode: 'string' }).defaultNow().notNull(),
+});
+
+export type CreateMemberDto = typeof members.$inferInsert;
+export type Member = typeof members.$inferSelect;
+
 export const dbhealth = pgTable("dbhealth", {
 	id: serial("id").primaryKey(),
 	name: text("name"),
