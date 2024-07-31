f28959332db772e6567c3c47eb7e0dda23b5b510
Eric Fuller <info@glassmoonmedia.com>
2024-07-31 17:51:11 -0400

    Import create member command from the proper location

--- packages/api/src/modules/journals/journal.repo.ts
+++ packages/api/src/modules/journals/journal.repo.ts
@@ -1,5 +1,6 @@
 import { ApiResponse } from '@efuller/shared/src/api';
-import { CreateJournalCommand, JournalDto } from '@efuller/api/src/shared/persistence/drizzle/schema';
+import { JournalDto } from '@efuller/api/src/shared/persistence/drizzle/schema';
+import { CreateJournalCommand } from '@efuller/shared/src/modules/journals/commands';
 
 export interface JournalRepo {
   createJournal(user: CreateJournalCommand): Promise<ApiResponse<JournalDto | null>>;
