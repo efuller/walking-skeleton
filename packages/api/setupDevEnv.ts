b81c22e662e023a2620fab4df4440370e6aadc94
Eric Fuller <info@glassmoonmedia.com>
2024-08-05 19:41:15 -0400

    Add in a script for running migrations along with push

--- packages/api/setupDevEnv.ts
+++ packages/api/setupDevEnv.ts
@@ -1,12 +1,16 @@
 import path from 'path';
 import { startDockerDB } from '@efuller/shared/tests/utils/startDockerDB';
 import { setupEnvVars } from '@efuller/shared/tests/utils/setupEnvVars';
-import { generateDrizzleKit } from '@efuller/shared/tests/utils/generateDrizzleKit';
+import {
+  generateDrizzleKit,
+  migrateDrizzleKit
+} from '@efuller/shared/tests/utils/generateDrizzleKit';
 
 export default async function setupDevEnv() {
   const rootCwd = path.join(__dirname);
   await startDockerDB('docker-compose.develop.yml', rootCwd);
   setupEnvVars('.env.development'); // This doesn't seem to be working or setting env vars for the server.
   await generateDrizzleKit(rootCwd);
+  await migrateDrizzleKit(rootCwd);
 }
 setupDevEnv();
