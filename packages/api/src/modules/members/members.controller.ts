b2e6ae6be27c618959c51aa5980aa36d304b24f2
Eric Fuller <info@glassmoonmedia.com>
2024-08-09 08:53:27 -0400

    Create a member response that returns a member dto

--- packages/api/src/modules/members/members.controller.ts
+++ packages/api/src/modules/members/members.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 import { MembersService } from '@efuller/api/src/modules/members/members.service';
 import { CreateMemberCommand } from '@efuller/shared/src/modules/members/commands';
+import { ApiResponse } from '@efuller/shared/src/api';
+import { MemberDto } from '@efuller/api/src/modules/members/member.dto';
+
+type CreateMemberResponse = ApiResponse<MemberDto | null>;
 
 export class MembersController {
   constructor(private readonly membersService: MembersService) {}
@@ -26,10 +30,20 @@ export class MembersController {
 
     const result = await this.membersService.getMemberByEmail(email);
 
+    const response: CreateMemberResponse = {
+      error: false,
+      success: true,
+      data: result,
+    };
+
     if (!result) {
-      return res.status(500).json(result);
+      return res.status(500).json({
+        error: true,
+        success: false,
+        message: 'Member not found',
+      });
     }
 
-    res.status(200).json(result);
+    res.status(200).json(response);
   }
 }
