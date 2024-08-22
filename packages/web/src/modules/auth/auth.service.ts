851266cfc0bd048fabb3326820ed62142458b8b8
Eric Fuller <info@glassmoonmedia.com>
2024-08-21 20:32:44 -0400

    Return user after register and for getSession as well

--- packages/web/src/modules/auth/auth.service.ts
+++ packages/web/src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Authenticator } from '@/modules/auth/ports/authenticator.ts';
 import { ApiResponse } from '@efuller/shared/dist/api';
 import { UserLoginDto, UserRegisterDto } from '@/modules/auth/auth.controller.ts';
-import { AuthTokenResponsePassword } from '@supabase/supabase-js';
+import { AuthResponse, AuthTokenResponsePassword } from '@supabase/supabase-js';
 
 export class AuthService {
   constructor(private readonly authClient: Authenticator) {}
@@ -23,15 +23,41 @@ export class AuthService {
     };
   }
 
-  async register(user: UserRegisterDto): Promise<boolean> {
-    return await this.authClient.register(user);
+  async register(user: UserRegisterDto): Promise<ApiResponse<AuthResponse>> {
+    const result = await this.authClient.register(user);
+
+    if (result.error) {
+      return {
+        success: false,
+        data: result,
+        error: new Error(result.error.message),
+      };
+    }
+
+    return {
+      success: true,
+      data: result
+    };
   }
 
   async logout(): Promise<void> {
     await this.authClient.logout();
   }
 
-  async getSession(): Promise<ApiResponse<null>> {
-    return await this.authClient.refreshSession();
+  async getSession(): Promise<ApiResponse<AuthResponse>> {
+    const result = await this.authClient.refreshSession();
+
+    if (result.error) {
+      return {
+        success: false,
+        data: result,
+        error: new Error(result.error.message),
+      };
+    }
+
+    return {
+      success: true,
+      data: result
+    };
   }
 }
