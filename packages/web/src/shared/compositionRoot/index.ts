129b74f25bb384f371b2c61f29fa1477ade01860
Eric Fuller <info@glassmoonmedia.com>
2024-09-05 20:37:35 -0400

    Compose the journals module

--- packages/web/src/shared/compositionRoot/index.ts
+++ packages/web/src/shared/compositionRoot/index.ts
@@ -3,11 +3,13 @@ import { AppRouter } from '@/shared/router';
 import { MembersModule } from '@/modules/members/members.module.ts';
 import { ClientApi } from '@efuller/shared/src/api';
 import { AppConfig } from '@/shared/appConfig';
+import { JournalsModule } from '@/modules/journals/journals.module.ts';
 
 export class CompositionRoot {
   private static instance: CompositionRoot;
   private readonly authModule: AuthModule;
   private readonly membersModule: MembersModule;
+  private readonly journalsModule: JournalsModule;
   private readonly appRouter: AppRouter;
   private readonly clientApi: ClientApi;
   private readonly apiUrl = process.env.API_URL || 'http://localhost:3000';
@@ -16,6 +18,7 @@ export class CompositionRoot {
     this.authModule = new AuthModule(this.config);
     this.clientApi = ClientApi.create(this.apiUrl, this.config, this.authModule);
     this.membersModule = new MembersModule(this.config, this.clientApi.app.app.members);
+    this.journalsModule = new JournalsModule(this.config, this.clientApi.app.app.journals);
     this.appRouter = new AppRouter(this.authModule, this.membersModule);
   }
 
@@ -39,6 +42,10 @@ export class CompositionRoot {
 
   public getMembersModule() {
     return this.membersModule;
+  }
+
+  public getJournalsModule() {
+    return this.journalsModule;
   }
 
   public getAppRouter() {
