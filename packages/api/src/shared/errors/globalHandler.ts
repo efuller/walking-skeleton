576891cdf45a3d8d6701b8dc29ed6f6ca7bfbc55
Eric Fuller <info@glassmoonmedia.com>
2024-09-11 18:30:26 -0400

    Handle zod validation errors

--- packages/api/src/shared/errors/globalHandler.ts
+++ packages/api/src/shared/errors/globalHandler.ts
@@ -1,12 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import { Logger } from '@efuller/api/src/shared/logger/logger';
 import { ERRORS_EXCEPTIONS } from '@efuller/api/src/shared/errors/constants';
+import { ZodError } from 'zod';
 
 export class ErrorHandler {
   constructor(private readonly logger: Logger) {}
 
   handle() {
     return (err: Error, req: Request, res: Response, next: NextFunction) => {
+      if (err instanceof ZodError) {
+        return res.status(400).json({
+          error: ERRORS_EXCEPTIONS.VALIDATION_ERROR,
+          data: undefined,
+          success: false,
+          message: err.message,
+          stack: process.env.NODE_ENV === 'production' ? undefined : err.stack
+        });
+      }
+
       this.logger.error(`${err.name}: ${err.message}`, err, {
         method: req.method,
         path: req.path,
